5dbbdfe58c4524ab1dbd36dbfc04c1b3bb8d9c7a
futile <git@futile.eu>
2024-12-04 14:17:31 +0000

    word

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ fn page() -> Html {
             <p class="content">
                 { "hello, my name is futile. i am a weird concept from the internet (or a person from europe, depending on who you ask)." }<br /><br />
                 { "i like programming (mostly rust), computers, linux, malicious software like game hacks and malware, and any vehicle whether it drives or flies." }<br /><br />
-                { "everything i know about the aforementioned things is self-taught. i also speak both english and russian fluently." }<br /><br />
+                { "everything i know about the aforementioned things is self-taught. i also speak english and russian fluently." }<br /><br />
                 { "if you want to contact me, email " }<a class="click" href="mailto:me@futile.eu"> { "me@futile.eu" } </a><br /><br />
                 <a class="button shiny" onclick={hire}> { "hire me!" } </a>
             </p>
